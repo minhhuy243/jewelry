432e12f0e6828b1e193aae69b5c34c2fa9ec2049
Phat Tai <dptai7@gmail.com>
2021-08-25 11:55:22 +0700

    show message from API

--- src/main/resources/static/client/js/customer/register.js
+++ src/main/resources/static/client/js/customer/register.js
@@ -1,51 +1,156 @@
-document.getElementById("registerForm").addEventListener('submit', register);
+document.getElementById("registerForm").addEventListener('submit', validation);
 
-function register(e) {
+function validation(event) {
 
-    // Reverse date YYYY-MM-DD ==> DD-MM-YYYY
-    var birthday = document.getElementById("registerBirthday").value;
-    var splitString = birthday.split("-");
-    var reverseArray = splitString.reverse();
-    var birthdayJoinArray = reverseArray.join("-");
+    // --- Full name ---
+    var registerFullName = document.getElementById("registerFullName");
+    var errorRegisterFullName = document.getElementById("errorRegisterFullName");
 
-    var gender = document.getElementById("registerGender").value;
+    if (registerFullName.value.trim() === "") {
+        registerFullName.style.border = "solid 1px red";
+        errorRegisterFullName.innerHTML = "Nhập họ và tên !";
+    } else if (registerFullName.value.trim().length < 3 || registerFullName.value.trim().length > 50) {
+        registerFullName.style.border = "solid 1px red";
+        errorRegisterFullName.innerHTML = "Nhập họ và tên từ 3 - 50 ký tự !";
+    } else {
+        registerFullName.style.border = "solid 1px green";
+        errorRegisterFullName.innerHTML = "";
+    }
 
 
-    axios.post(baseUrl + "/customer/register", {
+    // --- Email ---
+    var registerEmail = document.getElementById("registerEmail");
+    var errorRegisterEmail = document.getElementById("errorRegisterEmail");
+    var email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-        fullName: $('#registerFullName').val(),
-        email: $('#registerEmail').val(),
-        password: $('#registerPassword').val(),
-        confirmPassword: $('#registerRePassword').val(),
-        gender: gender,
-        birthday: birthdayJoinArray,
-        phoneNumber: $('#registerPhoneNumber').val(),
-        address: $('#registerAddress').val()
+    if (registerEmail.value.trim() === "") {
+        registerEmail.style.border = "solid 1px red";
+        errorRegisterEmail.innerHTML = "Nhập email !";
+    } else if (!registerEmail.value.match(email) || registerEmail.value.length > 200) {
+        registerEmail.style.border = "solid 1px red";
+        errorRegisterEmail.innerHTML = "Email không đúng định dạng !";
+    } else {
+        registerEmail.style.border = "solid 1px green";
+        errorRegisterEmail.innerHTML = "";
+    }
 
-    }).then(function (response) {
-        console.log(response);
-        window.location = "/login";
-    })
-        .catch(function (error) {
-            console.log(error);
-            if (error.response.status === 404) {
-                console.log(error);
-            }
+
+    // --- Password ---
+    var registerPassword = document.getElementById("registerPassword");
+    var errorRegisterPassword = document.getElementById("errorRegisterPassword");
+    var registerRePassword = document.getElementById("registerRePassword");
+    var errorRegisterRePassword = document.getElementById("errorRegisterRePassword");
+
+    if (registerPassword.value.trim() === "") {
+        registerPassword.style.border = "solid 1px red";
+        errorRegisterPassword.innerHTML = "Nhập mật khẩu !";
+    } else if (registerPassword.value.trim().length < 8 || registerPassword.value.trim().length > 30) {
+        registerPassword.style.border = "solid 1px red";
+        errorRegisterPassword.innerHTML = "Mật khẩu từ 8 - 30 ký tự !";
+    } else {
+        registerPassword.style.border = "solid 1px green";
+        errorRegisterPassword.innerHTML = "";
+    }
+    // -- Re Password --
+    if (registerPassword.value !== "" && registerPassword.value !== registerRePassword.value) {
+        registerRePassword.style.border = "solid 1px red";
+        errorRegisterRePassword.innerHTML = "Mật khẩu nhập lại không chính xác !";
+    } else {
+        registerRePassword.style.border = "solid 1px green";
+        errorRegisterRePassword.innerHTML = "";
+    }
+
+
+    // --- Birthday ---
+    var birthday = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;   // FORMAT : YYYY-MM-DD
+    var registerBirthday = document.getElementById("registerBirthday");
+    var errorRegisterBirthday = document.getElementById("errorRegisterBirthday");
+
+    if (!registerBirthday.value.match(birthday)) {
+        registerBirthday.style.border = "solid 1px red";
+        errorRegisterBirthday.innerHTML = "Sinh nhật không đúng định dạng !";
+    } else {
+        registerBirthday.style.border = "solid 1px green";
+        errorRegisterBirthday.innerHTML = "";
+    }
+
+    // --- Phone number ---
+    var phoneNumber = /^((?=(0))[0-9]{10})$/;
+    var registerPhoneNumber = document.getElementById("registerPhoneNumber");
+    var errorRegisterPhoneNumber = document.getElementById("errorRegisterPhoneNumber");
+
+    if (!registerPhoneNumber.value.match(phoneNumber)) {
+        registerPhoneNumber.style.border = "solid 1px red";
+        errorRegisterPhoneNumber.innerHTML = "Số điện thoại không đúng định dạng !";
+    } else {
+        registerPhoneNumber.style.border = "solid 1px green";
+        errorRegisterPhoneNumber.innerHTML = "";
+    }
+
+    // --- Address ---
+    var registerAddress = document.getElementById("registerAddress");
+    var errorRegisterAddress = document.getElementById("errorRegisterAddress");
+
+    if (registerAddress.value.trim().length < 20 || registerAddress.value.trim().length > 100) {
+        registerAddress.style.border = "solid 1px red";
+        errorRegisterAddress.innerHTML = "Địa chỉ từ 20 - 100 ký tự!";
+    } else {
+        registerAddress.style.border = "solid 1px green";
+        errorRegisterAddress.innerHTML = "";
+    }
+
+    /**
+     Call API after validation form success
+     */
+    if (errorRegisterFullName.innerHTML === "" && errorRegisterEmail.innerHTML === "" && errorRegisterPassword.innerHTML === ""
+        && errorRegisterRePassword.innerHTML === "" && errorRegisterBirthday.innerHTML === ""
+        && errorRegisterPhoneNumber.innerHTML === "" && errorRegisterAddress.innerHTML === "") {
+
+        // Reverse date YYYY-MM-DD ==> DD-MM-YYYY
+        var splitBirthday = registerBirthday.value.split("-");
+        var reverseArray = splitBirthday.reverse();
+        var birthdayJoinArray = reverseArray.join("-");
+
+        var gender = document.getElementById("registerGender").value;
+
+        axios.post(baseUrl + "/customer/register", {
+
+            fullName: $('#registerFullName').val(),
+            email: $('#registerEmail').val(),
+            password: $('#registerPassword').val(),
+            confirmPassword: $('#registerRePassword').val(),
+            gender: gender,
+            birthday: birthdayJoinArray,
+            phoneNumber: $('#registerPhoneNumber').val(),
+            address: $('#registerAddress').val()
+
+        }).then(function () {
+            window.location = "/login";
+        }).catch(function (error) {
             if (error.response.status === 400) {
-                console.log(error);
+                var iterator = error.response.data.message.values();
+                for (let element of iterator) {
+                    if (element.includes("Email")) {
+                        registerEmail.style.border = "solid 1px red";
+                        errorRegisterEmail.innerHTML = "Email đã được sử dụng !";
+                    }
+                    if (element.includes("Số điện thoại")) {
+                        registerPhoneNumber.style.border = "solid 1px red";
+                        errorRegisterPhoneNumber.innerHTML = "Số điện thoại đã được sử dụng !";
+                    }
+                }
             }
         });
-    e.preventDefault();
+    } else {
+        if (errorRegisterEmail.innerHTML === "") {
+            registerEmail.style.border = "solid 1px green";
+            errorRegisterEmail.innerHTML = "";
+        }
+        if (registerPhoneNumber.style.border === "") {
+            registerPhoneNumber.style.border = "solid 1px green";
+            errorRegisterPhoneNumber.innerHTML = "";
+        }
+    }
 
-
+    event.preventDefault();
 }
-
-
-
-
-
-
-
-
-
-
